09449e03452aa1f6594f177a6b7d8570a3d142e9
drfajry <dr.fajry@gmail.com>
2026-06-02 13:31:01 +0300

    Add update and version checking functions to preload.js

--- preload.js
+++ preload.js
@@ -4,6 +4,12 @@ contextBridge.exposeInMainWorld('api', {
   // License
   verifyLicense:    (code)   => ipcRenderer.invoke('verify-license', code),
   checkLicense:     ()       => ipcRenderer.invoke('check-license'),
+  // Updates
+  checkUpdate:      ()       => ipcRenderer.invoke('check-update'),
+  getVersion:       ()       => ipcRenderer.invoke('get-version'),
+  openReleases:     ()       => ipcRenderer.invoke('open-releases'),
+  onUpdateAvailable:(cb)     => ipcRenderer.on('update-available',     (_, d) => cb(d)),
+  onUpdateNotAvail: (cb)     => ipcRenderer.on('update-not-available', (_, d) => cb(d)),
   // Auth
   getAuth:          ()       => ipcRenderer.invoke('get-auth'),
   startOAuth:       ()       => ipcRenderer.invoke('start-oauth'),
@@ -18,6 +24,7 @@ contextBridge.exposeInMainWorld('api', {
   deleteScheduled:  (id)     => ipcRenderer.invoke('delete-scheduled', id),
   getHistory:       ()       => ipcRenderer.invoke('get-history'),
   fetchBestsellers: (data)   => ipcRenderer.invoke('fetch-bestsellers', data),
+  fetchProductImage:(url)    => ipcRenderer.invoke('fetch-product-image', url),
   fetchTrends:      (data)   => ipcRenderer.invoke('fetch-trends', data),
   onScheduledPosted:(cb)     => ipcRenderer.on('scheduled-posted', (_, data) => cb(data)),
   onScheduledFailed:(cb)     => ipcRenderer.on('scheduled-failed', (_, data) => cb(data)),
