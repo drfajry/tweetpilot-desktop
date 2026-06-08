71208ac632e7fdadac22f91c601f6ea3ee9e0155
drfajry <dr.fajry@gmail.com>
2026-06-08 08:46:07 +0300

    Remove unused authentication and Puppeteer callbacks

--- preload.js
+++ preload.js
@@ -20,13 +20,10 @@ contextBridge.exposeInMainWorld('api', {
   getAuth:          ()       => ipcRenderer.invoke('get-auth'),
   startOAuth:       ()       => ipcRenderer.invoke('start-oauth'),
   logout:           ()       => ipcRenderer.invoke('logout'),
-  onAuthSuccess:    (cb)     => ipcRenderer.on('auth-success', (_, data) => cb(data)),
-  onAuthError:      (cb)     => ipcRenderer.on('auth-error',   (_, err)  => cb(err)),
   // Puppeteer
   puppeteerPost:    (data)   => ipcRenderer.invoke('puppeteer-post', data),
   puppeteerLogin:   ()       => ipcRenderer.invoke('puppeteer-login'),
   checkChrome:      ()       => ipcRenderer.invoke('check-chrome'),
-  onPuppeteerInstalling: (cb) => ipcRenderer.on('puppeteer-installing', () => cb()),
   // Tweet
   generateTweet:    (data)   => ipcRenderer.invoke('generate-tweet', data),
   postTweet:        (data)   => ipcRenderer.invoke('post-tweet', data),
